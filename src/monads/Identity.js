e034c79b01a5b813a31977c29acc32e74484ce81
Tsubasa Namiki <namikingsoft@gmail.com>
2016-06-08 05:07:42 +0900

    refactor: identity monad class

--- src/monads/Identity.js
+++ src/monads/Identity.js
@@ -1,26 +1,11 @@
 // @flow
+import SingleValue from 'utils/SingleValue';
 import type { Monad } from 'types/Monad';
 
-class PrivateIdentity<T> {
-  _value: T;
-
-  constructor(value: T) {
-    this._value = value;
-  }
-
-  valueOf(): any {
-    return this._value;
-  }
-}
-
-export default class Identity<T> extends PrivateIdentity<T> {
+export default class Identity<T> extends SingleValue<T> {
 
   valueOf(): any {
     return this;
-  }
-
-  toString(): string {
-    return `Identity(${super.valueOf()})`;
   }
 
   bind<U>(transform: (value: T) => Monad<U>): Monad<U> {
