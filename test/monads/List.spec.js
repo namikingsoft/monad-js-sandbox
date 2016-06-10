3f0dff767ea3deee7ac43da96c6881ad51963f63
Tsubasa Namiki <namikingsoft@gmail.com>
2016-06-10 23:55:10 +0900

    chore: fix to failed eslint

--- test/monads/List.spec.js
+++ test/monads/List.spec.js
@@ -2,7 +2,7 @@
 import assert from 'power-assert';
 
 function* bind(list, transform) {
-  for (var item of list) {
+  for (const item of list) {
     yield* transform(item);
   }
 }
@@ -10,13 +10,13 @@ function* bind(list, transform) {
 describe('List', () => {
   describe('try', () => {
     it('should be success', () => {
-      const result = bind([0, 1, 2], function (x) {
-        return bind([0, 1, 2], function* (y) {
+      const result = bind([0, 1, 2], (x) =>
+        bind([0, 1, 2], function* (y) {
           yield x + y;
-        });
-      });
-      let items = [];
-      for (var item of result) {
+        })
+      );
+      const items = [];
+      for (const item of result) {
         items.push(item);
       }
       assert.deepEqual(items, [0, 1, 2, 1, 2, 3, 2, 3, 4]);
